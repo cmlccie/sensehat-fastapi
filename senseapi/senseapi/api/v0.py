59e0e0591ccf8bb26297987618935cfa3d2054b0
Chris Lunsford <chris@cmlccie.com>
2020-02-02 23:34:50 -0500

    Initial LED Matrix message endpoint

--- senseapi/senseapi/api/v0.py
+++ senseapi/senseapi/api/v0.py
@@ -1,6 +1,7 @@
 """Sense HAT FastAPI v0."""
 
 from enum import Enum
+from typing import List
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -88,4 +89,39 @@ def get_temperature() -> TemperatureReading:
         units=TemperatureUnit.celsius,
         humidity_sensor=sense.get_temperature_from_humidity(),
         pressure_sensor=sense.get_temperature_from_pressure(),
+    )
+
+
+@api.post(
+    "/message",
+    tags=["LED Matrix"],
+)
+def show_message(
+    text_string: str,
+    scroll_speed: float = 0.1,
+    text_colour: List[int] = (255, 255, 255),
+    back_colour: List[int] = (0, 0, 0),
+):
+    """Scroll a text message from right to left across the LED matrix.
+
+    Scrolls a text message from right to left across the LED matrix and at the
+    specified speed, in the specified colour and background colour.
+
+    Args:
+        text_string: The message to scroll.
+        scroll_speed: The speed at which the text should scroll. This value
+            represents the time paused for between shifting the text to the
+            left by one column of pixels. Defaults to 0.1
+        text_colour: A list containing the R-G-B (red, green, blue) colour of
+            the text. Each R-G-B element must be an integer between 0 and 255.
+            Defaults to [255, 255, 255] white.
+        back_colour: A list containing the R-G-B (red, green, blue) colour of
+            the background. Each R-G-B element must be an integer between 0 and
+            255. Defaults to [0, 0, 0] black / off.
+    """
+    sense.show_message(
+        text_string=text_string,
+        scroll_speed=scroll_speed,
+        text_colour=text_colour,
+        back_colour=back_colour,
     )
